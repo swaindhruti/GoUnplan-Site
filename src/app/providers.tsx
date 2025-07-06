5d3f0b57fec37b64eb03ae089d12e5aed484af16
AYANscyy2 <mayan6378@gmail.com>
2025-07-06 12:51:49 +0530

    fix: build error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,9 @@
 // app/providers.tsx
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+// import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-import { usePostHog } from "posthog-js/react";
+// import { usePostHog } from "posthog-js/react";
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
@@ -13,7 +13,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host:
         process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      person_profiles: "identified_only",
       defaults: "2025-05-24"
     });
   }, []);
